102cf4799519a90c9968271da536a33f107611d1
wailoc25 <frostwl25@gmail.com>
2025-01-01 22:52:43 +0800

    Update update_Cron.py

--- update_Cron.py
+++ update_Cron.py
@@ -13,7 +13,6 @@ cron.remove_all(comment='sensor_task')
 
 # Create a new cron job with a hidden comment
 job = cron.new(command='/home/pi/tms/bin/python3.7 /Desktop/Hortilite_Python/read_All_Devices.py >> /Desktop/Hortilite_Python/log/log.txt 2>&1', comment='sensor_task')
-job.setall('0 ' + intervalHour + '* * *')  # Every 6 hours
+job.setall('0 ' + intervalHour + '* * *')
 cron.write()
 
-# print("Cron job added with comment 'sensor_task'")
