c30ddb3785cc2bd8afab6e17701567ac779db08e
wailoc25 <frostwl25@gmail.com>
2025-01-02 16:12:20 +0800

    Update update_Cron.py

--- update_Cron.py
+++ update_Cron.py
@@ -4,9 +4,6 @@ from db_connect import get_data_retrieval_time
 # Initialize crontab for the current user
 cron = CronTab(user=True)
 
-for job in cron:
-    print(f"Command: {job.command}, Comment: {job.comment}")
-
 intervalHour = get_data_retrieval_time()
 
 cron.remove_all(comment='sensor_task')
@@ -16,3 +13,4 @@ job = cron.new(command='/home/pi/tms/bin/python3.7 /Desktop/Hortilite_Python/rea
 job.setall('0 ' + intervalHour + '* * *')
 cron.write()
 
+print("Cron job added with comment 'sensor_task'")
